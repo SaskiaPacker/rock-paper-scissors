de4e527645f12a409e45a8e33a6e99f65695317e
SaskiaPacker <>
2023-07-15 12:26:27 +0000

    Add compare combinations of games and which ones wins, loses and draws

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,36 @@ const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 
+
+// Get computer random choice//
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = (Math.floor(Math.random() * 3));
     return choices[randomNumber];
 }
 
+//gets combinations of the game and see which ones wines, looser or draws//
 function game(userChoice) {
-
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rockscissors":
+        case "paperrock":
+        case "sscissorspaper":
+            console.log("User Wins!");
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            console.log("User loses!");
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            console.log("It's a draw!");
+            break;
+    }
 }
+
 
 // Event listeners for when clicking the emoji buttons//
 function main() {
